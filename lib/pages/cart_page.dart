8529a1ad3c01ec4ff50c7e810268ed6312cfc01d
mihir_072 <you@example.com>
2024-02-19 23:51:17 +0530

    Day 9 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/themes.dart';
-import 'package:velocity_x/velocity_x.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/widgets/themes.dart';
+// import 'package:velocity_x/velocity_x.dart';
 
-class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+// class CartPage extends StatelessWidget {
+//   const CartPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: MyTheme.creamColor,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: 'Cart'.text.bold.make(),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: MyTheme.creamColor,
+//       appBar: AppBar(
+//         backgroundColor: Colors.transparent,
+//         title: 'Cart'.text.bold.make(),
+//       ),
+//     );
+//   }
+// }
